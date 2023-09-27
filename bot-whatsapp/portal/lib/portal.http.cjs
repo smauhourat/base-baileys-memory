32973cc844b348bf9e2f31e9a5062c3758784042
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-09-27 15:53:22 -0300

    bot local verbose

--- bot-whatsapp/portal/lib/portal.http.cjs
+++ bot-whatsapp/portal/lib/portal.http.cjs
@@ -21,6 +21,8 @@ const PUBLIC_URL = process.env.PUBLIC_URL ?? process.env.RAILWAY_STATIC_URL ?? '
 
 const dir = [join(__dirname, 'dist'), join(__dirname, '..', 'dist')].find((i) => existsSync(i));
 console.log('DIRECCION SERVIDOR WEB!!!!' + dir);
+console.log('HTTP_PORT:', HTTP_PORT);
+console.log('PUBLIC_URL:', PUBLIC_URL);
 const serve = require$$4(dir);
 
 /**
